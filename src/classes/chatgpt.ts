820d45b0b43bc0943af78bd8b26c00f6f47c89a9
PGRjoystick <30430815+PGRjoystick@users.noreply.github.com>
2025-03-13 07:12:55 +0000

    disable frequency, presence penalty if not defined

--- src/classes/chatgpt.ts
+++ src/classes/chatgpt.ts
@@ -49,8 +49,8 @@ class ChatGPT {
 			model: options?.model || "gpt-3.5-turbo", // default model
 			temperature: options?.temperature || 0.7,
 			max_tokens: options?.max_tokens || 100,
-			frequency_penalty: options?.frequency_penalty || 0,
-			presence_penalty: options?.presence_penalty || 0,
+			frequency_penalty: options?.frequency_penalty,
+			presence_penalty: options?.presence_penalty,
 			instructions: options?.instructions || `You are ChatGPT, a language model developed by OpenAI. You are designed to respond to user input in a conversational manner, Answer as concisely as possible. Your training data comes from a diverse range of internet text and You have been trained to generate human-like responses to various questions and prompts. You can provide information on a wide range of topics, but your knowledge is limited to what was present in your training data, which has a cutoff date of 2021. You strive to provide accurate and helpful information to the best of your ability.\nKnowledge cutoff: 2021-09`,
 			price: options?.price || 0.002,
 			max_conversation_tokens: options?.max_conversation_tokens || 4097,
